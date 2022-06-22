a35ce5f84571798d99c7cfad4309421d16c4e149
ProgRs <lfernandotc@hotmail.com>
2022-06-22 10:12:31 -0300

    Desenvolvimento dos layouts

--- app/src/main/java/me/dio/simulator/MainActivity.kt
+++ app/src/main/java/me/dio/simulator/MainActivity.kt
@@ -2,10 +2,22 @@ package me.dio.simulator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+//import me.dio.simulator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+   // private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+       // setContentView(R.layout.activity_main)
+       //val tvHello: TextView = findViewById<TextView>(R.id.tvHello)
+
+      //binding =  ActivityMainBinding.inflate(layoutInflater)
+      //setContentView(binding.root)
+      //binding.tvHello.text = "Teste"
     }
 }
